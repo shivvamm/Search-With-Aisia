e0621b93096220d42f7ee0d997472c1f6aad9ff5
shivvamm <shivam574488@gmail.com>
2024-10-11 15:09:50 +0530

    typing effect undefined glitch

--- client/src/components/effects/TypingEffect.jsx
+++ client/src/components/effects/TypingEffect.jsx
@@ -1,29 +1,25 @@
 import React, { useState, useEffect, useRef } from "react";
-import Markdown from "react-markdown";
+import ReactMarkdown from "react-markdown";
 
 const TypingEffect = ({ text, speed }) => {
-  const [displayedText, setDisplayedText] = useState("");
-  const textRef = useRef(null);
+  const [currentText, setCurrentText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    let index = 0;
+    if (currentIndex < text.length) {
+      const timeout = setTimeout(() => {
+        setCurrentText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, speed);
 
-    const timer = setInterval(() => {
-      if (index < text.length) {
-        setDisplayedText((prev) => prev + text[index]);
-        index += 1;
-      } else {
-        clearInterval(timer);
-      }
-    }, speed);
-
-    return () => clearInterval(timer);
-  }, [text, speed]);
+      return () => clearTimeout(timeout);
+    }
+  }, [currentIndex, speed, text]);
 
   return (
     <>
       <span className="text-pretty mt-4 text-sm text-neutral-600 dark:text-neutral-300">
-        <Markdown>{displayedText}</Markdown>
+        <ReactMarkdown>{currentText}</ReactMarkdown>
       </span>
     </>
   );
